c60f0edd5ad28de488c9d41ce0d73e814639e22a
Rod des Grenettes <rodrigue.bernard@gmail.com>
2020-12-18 01:20:09 +0100

    Update map.blade.php

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -9,21 +9,14 @@
 
         <!-- Fonts -->
        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        
-        
         <style>
             body {
                 font-family: 'Nunito';
-                
             }
-            
         </style>
-  
-
         @mapstyles
     </head>
     <body>
-        
         <div id="wrapper">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -39,94 +32,45 @@
                 </div>
             @endif
             
-            
             <h1>YAKAYALLER !</h1>
             <section>
-                
-    <article id="taille">
-        <ol>
-            @foreach ($events as $e)
-            <li>
-                <h2>{{ $e->title }}</h2>
-                <p>{{ $e->description }}</p>
-            </li>
-            @endforeach
-        </ol>
-    </article>
-                    <article id="carte">                
-                        @map([
-    'lat' => 48.134664,
-    'lng' => 11.555220,
-    'zoom' => 6,
-    'markers' => [
-        [
-            'title' => 'Go NoWare',
-            'lat' => 48.134664,
-            'lng' => 11.555220,
-            'url' => 'https://gonoware.com',
-            'icon' => 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            'icon_size' => [20, 32],
-            'icon_anchor' => [0, 32],
-        ],
-    ],
-])
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Event 1</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                                </div>
-                            </div>
-
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Event 2</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="text-center text-sm text-gray-500 sm:text-left">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
-
-                            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                                Shop
-                            </a>
-
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
-                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                            </svg>
-
-                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        dev : Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                        <br />
-                        yakayaller 2020 inc.
-                    </div>
-                </div>
+                <article id="taille">
+                    <ol>
+                        @foreach ($events as $e)
+                        <li>
+                            <h2>{{ $e->title }}</h2>
+                            <p>{{ $e->description }}</p>
+                        </li>
+                        @endforeach
+                    </ol>
+                </article>
+                    
+                <article id="carte">
+                    @map([
+                        'lat' => 48.134664,
+                        'lng' => 11.555220,
+                        'zoom' => 6,
+                        'markers' => [
+                            @foreach ($events as $e)
+                            [
+                                'title' => {{ $e->title }},
+                                'lat' => {{ $e->latitude }},
+                                'lng' => {{ $e->longitude }},
+                                'url' => {{ $e->link }},
+                                'icon' => 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                                'icon_size' => [20, 32],
+                                'icon_anchor' => [0, 32],
+                            ],
+                            @endforeach
+                        ],
+                    ])
+                </article>
+            </section>
+            <footer>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut doloremque temporibus incidunt? Repellendus eaque temporibus sit porro at! Repudiandae veritatis reprehenderit est consequatur odit commodi fuga unde recusandae reiciendis? Deleniti!</footer>
+            <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                dev : Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                <br />
+                yakayaller 2020 inc.
             </div>
         </div>
         @mapscripts
