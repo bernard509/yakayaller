5b545119695376332d642c1f81d11bdb3440daa3
Rod des Grenettes <rodrigue.bernard@gmail.com>
2021-01-01 15:31:14 +0100

    without over lapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,11 +25,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('log:cron')
+        $schedule->command('log:cron')->daily()->withoutOverlapping()->onOneServer();
             //->everyTwoHours();
             //->everyMinute();
             //->hourly();
-            ->daily();
+            // ->daily();
             //->twiceDaily(2, 13);
     }
 
