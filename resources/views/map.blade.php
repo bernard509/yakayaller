ec50aaee2071441796c69b09ad06436f65a4ce29
Rod des Grenettes <rodrigue.bernard@gmail.com>
2021-01-01 10:43:03 +0100

    seo unindex

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex, nofollow">
         <link rel="stylesheet" href="css/yakayaller.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
